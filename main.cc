5915e787de75052355a71f063497993e617711c4
muty-gif <muteerafi@gmail.com>
2025-10-20 18:49:58 +0200

    Score system implemented

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int SECRET_CODE;
+string SECRET_CODE;
 
 bool IS_VALID(int i) {
     if (i >= 12345 and i <= 98765) {
@@ -33,33 +35,113 @@ bool IS_VALID(int i) {
     return true;
 }
 
+string GAME_SCORE(string GUESS) {
+    string score = string(5, '-');
+    for (int i = 0; i < GUESS.length(); i++) {
+        for (int j = 0; j < SECRET_CODE.length(); j++) {
+            if (GUESS[i] == SECRET_CODE[j]) {
+                if (i == j) {
+                    score[i] = 'X'; 
+                }
+                else {
+                    score[i] = 'O';
+                }
+                break;
+            }
+        }
+    }
+
+    return score;
+}
+
 int main() {
+    srand(time(0));
     cout << "******* Welcome to Mastermind v.01 *******" << endl;
     cout << "Which mode do you wanna play? Manual [M] or Automatic [A]: ";
 
     bool f = false;
     char m;
     
-    while (!f) {
-        cin >> m;
-        if (m == 'M') {
-            while (true) {
-                cin >> SECRET_CODE;
-    
-                if (!IS_VALID(SECRET_CODE)) {
-                    cout << "Incorrect code. Try again." << endl;
-                }
-                else {
-                    cout << "Code introduced correctly. " << endl;
+    cin >> m;
+    if (m == 'M') {
+        while (true) {
+            cin >> SECRET_CODE;
+
+            bool numeric = true;
+
+            for (char c : SECRET_CODE) {
+                if (!isdigit(c)) {
+                    numeric = false;
                     break;
                 }
             }
+
+            if (!numeric or !IS_VALID(stoi(SECRET_CODE))) {
+                cout << "Incorrect code. Try again." << endl;
+            }
+            else {
+                cout << "Code introduced correctly. " << endl;
+                break;
+            }
         }
-        else if (m == 'A') {
-            //automatic mode
+    }
+    else if (m == 'A') {
+        int auxCode = 0;
+
+        while (true) {
+            auxCode = rand() % 86419 + 12345;
+            if (IS_VALID(auxCode)) {
+                break;
+            }
+        }
+
+        SECRET_CODE = to_string(auxCode);
+        cout << "Code generated succesfully!" << endl;
+    }
+    else {
+        cout << "Wrong input! Try again: " << endl;
+    }
+
+    bool END_GAME = false;
+    int COUNTER = 0;
+    string GUESS;
+
+    cout << "Game started. Make your guess: " << endl;
+    while (!END_GAME and COUNTER < 10) {
+        cin >> GUESS;
+        bool numeric = true;
+
+        for (char c : GUESS) {
+            if (!isdigit(c)) {
+                numeric = false;
+                break;
+            }
+        }
+
+        if (!numeric) {
+            cout << "Incorrect input. Try again. " << endl;
         }
         else {
-            cout << "Wrong input! Try again: " << endl;
+            if (!IS_VALID(stoi(GUESS))) {
+                cout << "Incorrect code. Try again. " << endl;
+            }
+            else {
+                cout << "Your guess: " << GUESS << endl;
+
+                string SCORE = GAME_SCORE(GUESS);
+
+                cout << "Score " << SCORE << endl;
+                
+                COUNTER++;
+
+                if (SCORE == "XXXXX") {
+                    END_GAME = true;
+                    cout << "You won!" << endl;
+                }
+                else if (COUNTER == 10) {
+                    cout << "You have no attempts left. The code was: " << SECRET_CODE << endl;
+                }
+            }
         }
     }
 }
